04f782aa7655de105b55dd835f907a14aad5f204
LucasAEscudero <lucasescudero5629@gmail.com>
2023-11-15 21:48:15 -0300

    without folder

--- api/src/routes/genresRouter.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const genresRouter = Router();
-
-const genresBDLoad = require('../handlers/getGenres');
-
-//routes
-genresRouter.get('/', genresBDLoad);
-
-module.exports = genresRouter;
